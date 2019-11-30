9982cea04da04ff46e8bbd2777df3bab9651d9b2
fachriezal <fachriezal.np@gmail.com>
2019-11-30 13:55:02 +0700

    update koneksi20

--- upload.php
+++ upload.php
@@ -8,10 +8,10 @@ $kategori= $_POST['kategori'];
 $logo='musikku2/gambar/'.$_FILES['foto']['name'];
 $logo=mysqli_real_escape_string($koneksi,$logo);
 
-if(preg_match("!image!", $_FILES['foto']['type']))
-{
-    if(copy($_FILES['foto']['tmp_name'], $logo))
-    {
+//if(preg_match("!image!", $_FILES['foto']['type']))
+//{
+  //  if(copy($_FILES['foto']['tmp_name'], $logo))
+   // {
         $sql="INSERT INTO lagu1 values('$id', '$logo' , '$judul', '$nama', '$kategori', '$alamat')";
         if(mysqli_query($koneksi,$sql))
         {
@@ -23,11 +23,11 @@ if(preg_match("!image!", $_FILES['foto']['type']))
             echo "<script>alert('Data Tidak Tersimpan')</script>";
         }
     }
-    else{
+    //else{
        // $result="gagal upload";
         echo "<script>alert('Data Tidak Tersimpan')</script>";
-    }
-}
+  //  }
+//}
 mysqli_close($koneksi);
 
 
