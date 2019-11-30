837b894b27a2c5dc0ce863142cc09844e139ed26
fachriezal <fachriezal.np@gmail.com>
2019-11-30 13:18:21 +0700

    update koneksi9

--- cek_admin.php
+++ cek_admin.php
@@ -12,8 +12,8 @@ $cek = mysqli_num_rows($data);
 if($cek > 0){
 	$_SESSION['username'] = $username;
 	$_SESSION['status'] = "login";
-	http_redirect("192.168.184.136/musikku2/input.php");
+	header("location:input.php");
 } else {
-	http_redirect("192.168.184.136/musikku2/loginadmin.php?pesan=gagal");
+	header("location:loginadmin.php?pesan=gagal");
 }
 ?>
