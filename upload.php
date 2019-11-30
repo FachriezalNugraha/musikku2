d1d45046e8c87c0d0b1a1e01ceae6be9787f8459
fachriezal <fachriezal.np@gmail.com>
2019-11-30 14:06:03 +0700

    update koneksi22

--- upload.php
+++ upload.php
@@ -8,13 +8,10 @@ $kategori= $_POST['kategori'];
 $logo='musikku2/gambar/'.$_FILES['foto']['name'];
 $logo=mysqli_real_escape_string($koneksi,$logo);
 
-//if(preg_match("!image!", $_FILES['foto']['type']))
-//{
-  //  if(copy($_FILES['foto']['tmp_name'], $logo))
-   // {
-        $sql="INSERT INTO lagu1 ('id', 'logo', 'nama', 'judul', 'kategori', 'alamat') values('$id', '$logo' , '$judul', '$nama', '$kategori', '$alamat')";
-        if(mysqli_query($koneksi,$sql))
-        {
+if(preg_match("!image!", $_FILES['foto']['type'])){
+    if(copy($_FILES['foto']['tmp_name'], $logo)){
+        $sql="INSERT INTO lagu1 values('$id', '$logo' , '$nama', '$judul', '$kategori', '$alamat')";
+        if(mysqli_query($koneksi,$sql)){
            // $result="Berhasil";
             echo "<script>alert('Data Tersimpan'); window.location='input.php'</script>";
         }
@@ -23,11 +20,16 @@ $logo=mysqli_real_escape_string($koneksi,$logo);
             echo "<script>alert('Data Tidak Tersimpan')</script>";
         }
     }
-    //else{
+    else{
        // $result="gagal upload";
         echo "<script>alert('Data Tidak Tersimpan')</script>";
-  //  }
-//}
+    }
+}
+else{
+    //$result="only upload jpg";
+    echo "<script>alert('Only Upload Photos!')</script>";
+}
+
 mysqli_close($koneksi);
 
 
