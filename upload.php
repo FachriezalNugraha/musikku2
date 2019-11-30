f276b53418c019f191cc871fd09b1d349ebe50ec
fachriezal <fachriezal.np@gmail.com>
2019-11-30 14:11:05 +0700

    update koneksi23

--- upload.php
+++ upload.php
@@ -8,7 +8,7 @@ $kategori= $_POST['kategori'];
 $logo='musikku2/gambar/'.$_FILES['foto']['name'];
 $logo=mysqli_real_escape_string($koneksi,$logo);
 
-if(preg_match("!image!", $_FILES['foto']['type'])){
+if(preg_match('/\.(jpg|png|jpeg)$/', $_FILES['foto']['type'])){
     if(copy($_FILES['foto']['tmp_name'], $logo)){
         $sql="INSERT INTO lagu1 values('$id', '$logo' , '$nama', '$judul', '$kategori', '$alamat')";
         if(mysqli_query($koneksi,$sql)){
