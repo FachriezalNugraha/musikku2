330f72e2bf501d3d3098d83414d56b49d48c43cc
fachriezal <fachriezal.np@gmail.com>
2019-12-02 00:07:19 +0700

    update konek19

--- loginadmin.php
+++ loginadmin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bakery</title>
+    <title>HOME MUSIK</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/-Login-form-Page-BS4-.css">
