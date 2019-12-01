8ba20d52093fc04e90cd31f768a7bbb6010350cf
fachriezal <fachriezal.np@gmail.com>
2019-12-01 23:29:47 +0700

    update konek11

--- edit.php
+++ edit.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 if(empty($_SESSION['username']))
@@ -14,7 +13,7 @@ if(empty($_SESSION['username']))
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOME MUSIK</title>
+    <title>bakery</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/-Login-form-Page-BS4-.css">
@@ -26,7 +25,7 @@ if(empty($_SESSION['username']))
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css"> 
     <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">    
 </head>
 
 <body>
@@ -37,21 +36,19 @@ if(empty($_SESSION['username']))
                     class="collapse navbar-collapse" id="navcol-1">
                     <ul class="nav navbar-nav ml-auto">
                         <li class="dropdown"><a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Menu&nbsp;</a>
-                            <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="input.php">Tambahkan Lagu</a><a class="dropdown-item" role="presentation" href="tabel_lagu.php">Daftar lagu</a></div>
+                            <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="input.php">Tambahkan Lagu</a><a class="dropdown-item" role="presentation" href="tabel_lagu.php">Daftar Lagu</a></div>
                         </li>
                         <li class="nav-item" role="presentation"><a class="nav-link" href="#"></a></li>
-                   <a class="btn btn-light action-button" role="button" href="logoutadmin.php">Logout</a></span></div>
-              
+                    <a class="btn btn-light action-button" role="button" href="Logoutadmin.php">Logout</a></span></div>
     </div>
     </nav>
     </div>
-    <div style="height: 450px;  width: auto; border-top-left-radius: 60px; border-top-right-radius: 60px; padding-left: 10px"  class="form-input">
+    <div style="height: 450px;  width: auto; background-color: #DEB887; border-top-left-radius: 60px; border-top-right-radius: 60px; padding-left: 10px"  class="form-input">
 <center>
-        <form method="POST" class="contact1-form validate-form" action="update.php" enctype="multipart/form-data">
+        <form method="POST" class="contact1-form validate-form" action="upload.php" enctype="multipart/form-data">
           <span class="contact1-form-title">
-       Tambahkan lagu
+          Get in touch
         </span>
- <table>
             <?php
       include 'konek.php';
       $id=$_GET['id'];
@@ -59,76 +56,42 @@ if(empty($_SESSION['username']))
     $data=mysqli_fetch_array($query);
       ?>
 
-            <tr>
-              <td>ID Product</td>
-              <td></td>
-              <td><input type="text" name="id" value="<?php echo $data['id']; ?>"></td>
-            </tr>
-            <tr>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Picture</td>
-              <td></td>
-              <td><input type="file" name="foto" value="<?php echo $data['gambar']; ?>"></td>
-            </tr>
-            <tr>
-              <td></td>
-            </tr>
-            <tr>
-                <tr>
-             <tr>
-                <tr>
-              <td>Nama Band</td>
-              <td>  </td>
-              <td><input type="text" name="nama" value="<?php echo $data['nama']; ?>"></td>
-            </tr>
-            <tr>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Judul</td>
-              <td></td>
-              <td><input type="text" name="judul" value="<?php echo $data['judul']; ?>"></td>
-            </tr>
-            <tr>
-              <td></td>
-            </tr>
-                <tr>
-              <td>Link File</td>
-              <td></td>
-              <td><input type="text" name="alamat" value="<?php echo $data['alamat']; ?>"></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-            </tr>
-        <tr>    
-          <td>Kategori jenis lagu</td>
-              <td></td>
-              <td><select name="kategori">
-                <option value="Lagu Indonesia"> Lagu Indonesia </option>
-                <option value="Lagu Luar"> Lagu Luar </option>
-                <option value="Religi"> Religi </option>    
-          </select>
-            </td>
-            </tr>      
-            
-              <td></td>
-              <td><input type="Submit" name="Submit" value="Submit"></td>
-            </tr>
 
-          </table>
+        <div class="wrap-input1 validate-input" data-validate = "ID Band is required">
+          <input class="input1" type="text" name="id" value="<?php echo $data['id']; ?>" placeholder="ID Band">
+          <span class="shadow-input1"></span>
+        </div>
 
-        </form>
-        </center>
+        <div class="wrap-input1 validate-input">
+          <input class="input1" type="file" name="foto"  value="<?php echo $data['gambar']; ?>">
+          <span class="shadow-input1"></span>
+        </div>
 
+        <div class="wrap-input1 validate-input" data-validate = "Nama Band is required">
+          <input class="input1" type="text" name="nama" placeholder="Nama Band">
+          <span class="shadow-input1"></span>
+        </div>
+
+        <div class="wrap-input1 validate-input" data-validate = "Judul lagu is required">
+          <input class="input1" name="judul" placeholder="Judul Lagu"></input>
+          <span class="shadow-input1"></span>
+        </div>
+        <div class="wrap-input1 validate-input" data-validate = "Link file is required">
+          <input class="input1" name="alamat" placeholder="Link file"></input>
+          <span class="shadow-input1"></span>
+        </div>
+        <div class="container-contact1-form-btn">
+          <button class="contact1-form-btn">
+            <span>
+              Submit
+              <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+      </form>
+    </center>
+           
     </div>
-
-     
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
