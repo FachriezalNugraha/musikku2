8f2206956e2d04afa4fd8b0e2c85d7c20007f8fd
fachriezal <fachriezal.np@gmail.com>
2019-12-02 00:43:51 +0700

    update update8

--- edit.php
+++ edit.php
@@ -45,7 +45,7 @@ if(empty($_SESSION['username']))
     </div>
     </nav>
     </div>
-    <div style="height: 450px;  width: auto; border-top-left-radius: 60px; border-top-right-radius: 60px; padding-left: 10px; background-color: 1db954;; class="form-input" ">
+    <div style="height: 450px;  width: auto; border-top-left-radius: 60px; border-top-right-radius: 60px; padding-left: 10px; background-color: 1db954; class="form-input" ">
 <center>
         <form method="POST" class="contact1-form validate-form" action="upload.php" enctype="multipart/form-data">
           <span class="contact1-form-title">
